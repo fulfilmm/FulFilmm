2d3faa80613219e92cc0a99d237a8f9865f5a822
Htet Linn Oo <hloo@onesmartstar.com.mm>
2020-11-06 19:02:23 +0630

    department crud completed

---
+++ resources/lang/en/alert.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    //Alert messages
+    'create_success' => 'Record is created Successfully.',
+    'update_success' => 'Record is updated Successfully.',
+    'delete_success' => 'Record is deleted Successfully.'
+];
