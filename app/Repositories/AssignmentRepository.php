005281b3b7b7adcd560bfd9b7e73e357814dfc74
Nay Linn Oo <naylinnoo.1@gmail.com>
2021-05-26 10:04:56 +0630

    progress bar

--- app/Repositories/AssignmentRepository.php
+++ app/Repositories/AssignmentRepository.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace App\Repositories;
 
 use App\Models\Assignment;
 use App\Repositories\Contracts\AssignmentContract;
+use Illuminate\Database\Eloquent\Builder;
 
 class AssignmentRepository extends BaseRepository implements AssignmentContract
 {
@@ -13,6 +15,12 @@ class AssignmentRepository extends BaseRepository implements AssignmentContract
 
     public function getAssignmentsWithTasks($assignment_id)
     {
-        return $this->model->with('assignment_tasks')->find($assignment_id);
+        return $this->model->with('assignment_tasks')
+            ->withCount(['assignment_tasks as task_done' => function (Builder $q) {
+                $q->where('status', 1);
+            }])
+            ->withCount(['assignment_tasks as total_tasks'])
+            ->find($assignment_id);
     }
+
 }
