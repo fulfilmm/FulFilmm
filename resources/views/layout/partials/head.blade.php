bb94b86caac5fc370279b4a657c6dcd8530aa349
CinPu <cincin.com@gmail.com>
2022-02-15 17:57:41 +0630

    update

--- resources/views/layout/partials/head.blade.php
+++ resources/views/layout/partials/head.blade.php
@@ -131,7 +131,7 @@
 
         {{--</script>--}}
         <link rel="stylesheet" href="{{url(asset('css/mdtimepicker.css'))}}">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>--}}
         <style>
             body,head,header,h3,.card-title{
                 font-family: 'Inter';
