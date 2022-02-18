725d22465feb83750187f8d62dd311acdaa93d08
CinPu <cincin.com@gmail.com>
2022-02-18 21:51:46 +0630

    update

--- app/Models/SellingUnit.php
+++ app/Models/SellingUnit.php
@@ -10,6 +10,6 @@ class SellingUnit extends Model
     use HasFactory;
     protected $fillable=['product_id','unit','unit_convert_rate','active'];
     public function variant(){
-        return $this->belongsTo(product::class,'variant_id','id');
+        return $this->belongsTo(product::class,'product_id','id');
     }
 }
