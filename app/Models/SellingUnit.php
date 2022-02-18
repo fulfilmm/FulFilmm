5dbffb712f08bdfcd00dc1f3008fd9dbb029c464
CinPu <cincin.com@gmail.com>
2022-02-18 21:50:21 +0630

    update

--- app/Models/SellingUnit.php
+++ app/Models/SellingUnit.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SellingUnit extends Model
 {
     use HasFactory;
-    protected $fillable=['variant_id','unit','unit_convert_rate','active'];
+    protected $fillable=['product_id','unit','unit_convert_rate','active'];
     public function variant(){
         return $this->belongsTo(product::class,'variant_id','id');
     }
