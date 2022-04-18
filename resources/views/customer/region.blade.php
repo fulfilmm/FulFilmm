f76baa579b46f603114cf9abe518ec3657f7a42b
CinPu <cincin.com@gmail.com>
2022-04-18 12:12:57 +0630

    18Aprol2022

--- resources/views/customer/region.blade.php
+++ resources/views/customer/region.blade.php
@@ -17,69 +17,77 @@
             </div>
         </div>
         <div class="col-12">
-            <table class="table table-nowrap table-hover">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Branch</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($regions as $item)
-                    <tr>
-                        <td>{{$item->name}}</td>
-                        <td>{{$item->branch->name}}</td>
-                        <td>
-                            <div class="row">
-                                <button type="button" data-toggle="modal" data-target="#edit_region{{$item->id}}" class="btn btn-success btn-sm"><i class="la la-edit"></i></button>
-                                <div id="edit_region{{$item->id}}" class="modal custom-modal fade" role="dialog">
-                                    <div class="modal-dialog modal-dialog-centered modal-sm">
-                                        <div class="modal-content">
-                                            <div class="modal-header border-bottom">
-                                                <h5 class="modal-title">Edit Sale Zone</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form action="{{route('region.update',$item->id)}}" method="post">
-                                                    @csrf
-                                                    @method('PUT')
-                                                    <div class="form-group">
-                                                        <label for="name">Name</label>
-                                                        <input type="text" class="form-control" name="name" value="{{$item->name}}">
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="branch">Office Branch</label>
-                                                        <select name="branch_id" id="branch" class="form-control select2" style="width: 100%">
-                                                            @foreach($branch as $key=>$val)
-                                                                <option value="{{$key}}" {{$item->branch_id==$key?'selected':''}}>{{$val}}</option>
-                                                            @endforeach
-                                                        </select>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <form action="{{route('region.destroy',$item->id)}}" method="post">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="la la-trash"></i></button>
-                                </form>
-                            </div>
+            <div class="card shadow">
+               <div class="col-12 my-2">
+                   <table class="table table-nowrap table-hover" id="region">
+                       <thead>
+                       <tr>
+                           <th>Name</th>
+                           <th>Branch</th>
+                           <th>Action</th>
+                       </tr>
+                       </thead>
+                       <tbody>
+                       @foreach($regions as $item)
+                           <tr>
+                               <td>{{$item->name}}</td>
+                               <td>{{$item->branch->name}}</td>
+                               <td>
+                                   <div class="row">
+                                       <button type="button" data-toggle="modal" data-target="#edit_region{{$item->id}}" class="btn btn-success btn-sm"><i class="la la-edit"></i></button>
+                                       <div id="edit_region{{$item->id}}" class="modal custom-modal fade" role="dialog">
+                                           <div class="modal-dialog modal-dialog-centered modal-sm">
+                                               <div class="modal-content">
+                                                   <div class="modal-header border-bottom">
+                                                       <h5 class="modal-title">Edit Sale Zone</h5>
+                                                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                           <span aria-hidden="true">&times;</span>
+                                                       </button>
+                                                   </div>
+                                                   <div class="modal-body">
+                                                       <form action="{{route('region.update',$item->id)}}" method="post">
+                                                           @csrf
+                                                           @method('PUT')
+                                                           <div class="form-group">
+                                                               <label for="name">Name</label>
+                                                               <input type="text" class="form-control" name="name" value="{{$item->name}}">
+                                                           </div>
+                                                           <div class="form-group">
+                                                               <label for="branch">Office Branch</label>
+                                                               <select name="branch_id" id="branch" class="form-control select2" style="width: 100%">
+                                                                   @foreach($branch as $key=>$val)
+                                                                       <option value="{{$key}}" {{$item->branch_id==$key?'selected':''}}>{{$val}}</option>
+                                                                   @endforeach
+                                                               </select>
+                                                           </div>
+                                                           <div class="form-group">
+                                                               <button type="submit" class="btn btn-primary">Submit</button>
+                                                           </div>
+                                                       </form>
+                                                   </div>
+                                               </div>
+                                           </div>
+                                       </div>
+                                       <form action="{{route('region.destroy',$item->id)}}" method="post">
+                                           @csrf
+                                           @method('delete')
+                                           <button type="submit" class="btn btn-danger btn-sm"><i class="la la-trash"></i></button>
+                                       </form>
+                                   </div>
 
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-
+                               </td>
+                           </tr>
+                       @endforeach
+                       </tbody>
+                   </table>
+               </div>
+            </div>
         </div>
     </div>
     @include('customer.addregion')
+    <script>
+        $(document).ready(function () {
+            $('#region').DataTable();
+        })
+    </script>
 @endsection
--- resources/views/customer/salezone.blade.php
+++ resources/views/customer/salezone.blade.php
@@ -17,69 +17,78 @@
             </div>
         </div>
         <div class="col-12">
-            <table class="table table-nowrap table-hover">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Region</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($zones as $zone)
-                    <tr>
-                        <td>{{$zone->name}}</td>
-                        <td>{{$zone->region->name}}</td>
-                        <td>
-                            <div class="row">
-                                <button type="button" data-toggle="modal" data-target="#edit_zone{{$zone->id}}" class="btn btn-success btn-sm"><i class="la la-edit"></i></button>
-                                <div id="edit_zone{{$zone->id}}" class="modal custom-modal fade" role="dialog">
-                                    <div class="modal-dialog modal-dialog-centered modal-sm">
-                                        <div class="modal-content">
-                                            <div class="modal-header border-bottom">
-                                                <h5 class="modal-title">Edit Sale Zone</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form action="{{route('salezone.update',$zone->id)}}" method="post">
-                                                    @csrf
-                                                    @method('PUT')
-                                                    <div class="form-group">
-                                                        <label for="name">Name</label>
-                                                        <input type="text" class="form-control" name="name" value="{{$zone->name}}">
+            <div class="card shadow">
+                <div class="col-12 my-2">
+                    <table class="table table-nowrap table-hover" id="salezone">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Region</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($zones as $zone)
+                            <tr>
+                                <td>{{$zone->name}}</td>
+                                <td>{{$zone->region->name}}</td>
+                                <td>
+                                    <div class="row">
+                                        <button type="button" data-toggle="modal" data-target="#edit_zone{{$zone->id}}" class="btn btn-success btn-sm"><i class="la la-edit"></i></button>
+                                        <div id="edit_zone{{$zone->id}}" class="modal custom-modal fade" role="dialog">
+                                            <div class="modal-dialog modal-dialog-centered modal-sm">
+                                                <div class="modal-content">
+                                                    <div class="modal-header border-bottom">
+                                                        <h5 class="modal-title">Edit Sale Zone</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
                                                     </div>
-                                                    <div class="form-group">
-                                                        <label for="region">Region</label><br>
-                                                        <select name="region_id" id="region{{$zone->id}}" class="form-control select2" style="width: 100%">
-                                                            @foreach($region as $key=>$val)
-                                                                <option value="{{$key}}" {{$key==$zone->region_id?'selected':''}}>{{$val}}</option>
-                                                            @endforeach
-                                                        </select>
+                                                    <div class="modal-body">
+                                                        <form action="{{route('salezone.update',$zone->id)}}" method="post">
+                                                            @csrf
+                                                            @method('PUT')
+                                                            <div class="form-group">
+                                                                <label for="name">Name</label>
+                                                                <input type="text" class="form-control" name="name" value="{{$zone->name}}">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="region">Region</label><br>
+                                                                <select name="region_id" id="region{{$zone->id}}" class="form-control select2" style="width: 100%">
+                                                                    @foreach($region as $key=>$val)
+                                                                        <option value="{{$key}}" {{$key==$zone->region_id?'selected':''}}>{{$val}}</option>
+                                                                    @endforeach
+                                                                </select>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <button type="submit" class="btn btn-primary">Submit</button>
+                                                            </div>
+                                                        </form>
                                                     </div>
-                                                    <div class="form-group">
-                                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                                    </div>
-                                                </form>
+                                                </div>
                                             </div>
                                         </div>
+                                        <form action="{{route('salezone.destroy',$zone->id)}}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit" class="btn btn-danger btn-sm"><i class="la la-trash"></i></button>
+                                        </form>
                                     </div>
-                                </div>
-                                <form action="{{route('salezone.destroy',$zone->id)}}" method="post">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="la la-trash"></i></button>
-                                </form>
-                            </div>
 
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
         </div>
     </div>
   @include('customer.addzone')
+    <script>
+        $(document).ready(function () {
+           $('#salezone').DataTable();
+        });
+    </script>
 @endsection
