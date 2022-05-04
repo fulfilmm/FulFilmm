4fb95ab4edd4a595fb24e3f1b477a2ddca810a68
CinPu <cincin.com@gmail.com>
2022-05-04 11:36:57 +0630

    update stock table

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -7,12 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    protected $fillable=['variant_id','product_name','stock_balance','available','warehouse_id','alert_qty','product_location'];
+    protected $fillable=['variant_id','product_name','product_id','stock_balance','available','warehouse_id','alert_qty','product_location'];
     use HasFactory;
     public function warehouse(){
         return $this->belongsTo(Warehouse::class,'warehouse_id','id');
     }
     public function variant(){
         return $this->belongsTo(ProductVariations::class,'variant_id','id');
+    }
+    public function product(){
+        return $this->belongsTo(product::class,'product_id','id');
     }
 }
