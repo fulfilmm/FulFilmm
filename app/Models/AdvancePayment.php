8e8f0cf2fd6582c651695977b1df0a47e8e5eaea
CinPu <cincin.com@gmail.com>
2022-05-27 12:07:09 +0630

    27may2022 advance payment update

--- app/Models/AdvancePayment.php
+++ app/Models/AdvancePayment.php
@@ -8,17 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class AdvancePayment extends Model
 {
     use HasFactory;
-    protected $fillable=['order_id','amount','emp_id','account_id','customer_id','type','payment_type','transaction_done'];
+    protected $fillable=['amount','emp_id','customer_id','payment_type','approver_id'];
     public function customer(){
         return $this->belongsTo(Customer::class,'customer_id','id');
     }
-    public function order(){
-        return $this->belongsTo(Order::class,'order_id','id');
-    }
+
     public function emp(){
         return $this->belongsTo(Employee::class,'emp_id','id');
     }
-    public function account(){
-        return $this->belongsTo(Account::class,'account_id','id');
+
+    public function approver(){
+        return $this->belongsTo(Employee::class,'approver_id','id');
     }
 }
