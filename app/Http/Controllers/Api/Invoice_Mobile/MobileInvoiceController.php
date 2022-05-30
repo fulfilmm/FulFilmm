d667b48b217f0e9684d34a14e632806843bcbcd7
CinPu <cincin.com@gmail.com>
2022-05-30 11:58:54 +0630

    updated api invoice 30may2022

--- app/Http/Controllers/Api/Invoice_Mobile/MobileInvoiceController.php
+++ app/Http/Controllers/Api/Invoice_Mobile/MobileInvoiceController.php
@@ -87,7 +87,12 @@ class MobileInvoiceController extends Controller
                 ->where('start_date','<=',Carbon::today())->where('end_date','>=',Carbon::today())
                 ->where('region_id',$Auth->region_id)
                 ->get();
-            $focs = Freeofchare::with('variant')->where('branch_id',$Auth->office_branch_id)->get();
+           $foc_item = Freeofchare::with('variant')->where('branch_id',$Auth->office_branch_id)->get();
+            $focs=[];
+           foreach ($foc_item as $item){
+                $focs->unit=SellingUnit::where('product_id',$item->variant->product_id)->get();
+                array_push($focs,$item);
+            }
             $type = 'Whole Sale';
 
             if(Auth::guard('api')->user()->mobile_seller==1){
