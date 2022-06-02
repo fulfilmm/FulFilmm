e9cc13eca645b5861f87ca789490befd9e9a370c
CinPu <cincin.com@gmail.com>
2022-06-02 15:03:03 +0630

    update

--- app/Http/Controllers/Api/RevenueController.php
+++ app/Http/Controllers/Api/RevenueController.php
@@ -55,10 +55,10 @@ class RevenueController extends Controller
             'payment_method' => 'required',
             'approver_id'=>'required',
             'currency'=>'required',
-            'attachment' => 'mimes:pdf,xlsx,doc,docx,jpg,jpeg,ppt,bip',
+//            'attachment' => 'mimes:pdf,xlsx,doc,docx,jpg,jpeg,ppt,bip',
 //                'coa_account'=>'required'
         ]);
-        return response()->json(['data'=>$request->all()]);
+
 
         try {
             $branch_acc=Account::where('branch_id',Auth::guard('api')->user()->office_branch_id)->first();
