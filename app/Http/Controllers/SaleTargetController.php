0402088335fd451542c36d8db92a24c5ba90d62e
CinPu <cincin.com@gmail.com>
2022-06-05 12:42:56 +0630

    5Jun2022 Update

--- app/Http/Controllers/SaleTargetController.php
+++ app/Http/Controllers/SaleTargetController.php
@@ -384,15 +384,17 @@ class SaleTargetController extends Controller
     }
     public function show($id){
         $sale_target=SaleTarget::with('employee')->where('id',$id)->first();
-        $items=SaletargetItem::with('product')->where('sale_target_id',$id)->get();
+        $tartget_items=SaletargetItem::with('product')->where('sale_target_id',$id)->get();
 //        dd($items);
         $invoices=Invoice::where('emp_id',$sale_target->emp_id)->whereMonth('created_at',$sale_target->month)->get();
         foreach ($invoices as $inv){
-            foreach ($items as $item) {
+        $items=[];
+            foreach ($tartget_items as $item) {
                 $order_item=OrderItem::with('unit')->where('variant_id',$item->item_id)->where('inv_id',$inv->id)->get();
                 foreach ($order_item as $or_item){
 //                $item->sold_qty+=1;
                     $item->sold_qty+=$or_item->qty*$or_item->unit->unit_convert_rate;
+                    array_push($items,$item);
                 }
             }
         }
