56e265ebe4c2a12c00d532df26c1dc1a31252ad0
CinPu <cincin.com@gmail.com>
2022-06-30 15:33:10 +0630

    update

--- resources/views/sale/SaleWay/Shop/show.blade.php
+++ resources/views/sale/SaleWay/Shop/show.blade.php
@@ -28,7 +28,7 @@
                 <div class="col-md-9 col-12">
                     <div id="map" style="width: 750px; height: 550px;"></div>
                 </div>
-                <div class="col-md-3 col-12 ">
+                <div class="col-md-3 col-12 border">
                         <div class="card">
                             <img src="{{url(asset('img/profiles/'.$shop->picture))}}" alt="" width="80%" height="50%">
                         </div>
