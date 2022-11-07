716267505d4a1f3fe9b7bb9d05bf97759454ea32
CinPu <cincin.com@gmail.com>
2022-11-07 17:09:33 +0630

    updated at 7Nov2022

--- app/Http/Controllers/Api/ExpenseClaimController.php
+++ app/Http/Controllers/Api/ExpenseClaimController.php
@@ -75,7 +75,7 @@ class ExpenseClaimController extends Controller
             }
             $items=json_decode($request->items);
             $cc=json_decode($request->tag);
-            return response()->json(['data'=>$cc]);
+            return response()->json(['data'=>$cc[0]]);
             $exp_claim->save();
             $this->addnotify($request->finance_approver, 'success', 'Request to expense claim.', 'expenseclaims/' . $exp_claim->id, Auth::guard('api')->user()->id);
             $this->addnotify($request->approver, 'success', 'Request to expense claim.', 'expenseclaims/' . $exp_claim->id, Auth::guard('api')->user()->id);
