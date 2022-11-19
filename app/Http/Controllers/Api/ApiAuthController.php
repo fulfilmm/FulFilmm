c7410b38cdb3c7d70f3830c1e40a3bed1fbf6d58
CinPu <cincin.com@gmail.com>
2022-11-19 16:15:06 +0630

    updated at 19Nov2022

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -30,7 +30,7 @@ class ApiAuthController extends Controller
         $credentials = request(['email', 'password']);
         $token = auth('api')->attempt($credentials);
         if (!$token) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['con'=>false,'error' => 'Unauthorized'], 401);
         }
         $result=['token'=>$token,'id'=>\auth('api')->user()->id,'name'=>\auth('api')->user()->name,'email'=>\auth('api')->user()->email,'phone'=>\auth('api')->user()->phone,'profile_img'=>\auth('api')->user()->profile_img];
         return response()->json([
