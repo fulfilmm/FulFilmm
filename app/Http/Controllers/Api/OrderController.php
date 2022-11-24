72a454fdff0069a468666e6ae992ae592921d985
CinPu <cincin.com@gmail.com>
2022-11-24 15:15:46 +0630

    updated at 24Nov2022

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Employee;
 use App\Models\Order;
 use App\Models\OrderCc;
 use App\Models\OrderItem;
+use App\Models\ProductVariations;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -88,27 +89,25 @@ class OrderController extends Controller
             $order->status="New";
             $order->emp_id=Auth::guard('api')->user()->id??$request->approver_id;
             $order->order_date = Carbon::create($request->order_date);
+            $order->save();
+            $order_item = $request->order_items;
+            $foc=$request->foc_items??[];
+            if(count($order_item)!=0){
+                foreach ($order_item as $item){
 
-            if(Auth::guard('employee')->check()){
-                $Auth="order-".Auth::guard('employee')->user()->name;
-            }else{
-                $Auth= "order-" .Auth::guard('customer')->user()->name;
+                    $item['order_id']=$order->id;
+                    $item['type']='invoice';
+                    $this->item_store($item);
+
+                }
             }
-            $request_id=Session::get($Auth);
-//            dd($request_id);
-            $confirm_order_item=OrderItem::where("creation_id",$request_id)->get();
-            if(count($confirm_order_item)==0){
-                return response()->json(['orderempty'=>'Order Item Empty']);
-            }else{
-                $order->save();
+            if(count($foc)!=0){
+                foreach ($foc as $item){
+                    $item->invoice_id=$order->id;
+                    $item->type='invoice';
+                    $this->foc_add($item);
+                }
             }
-//            dd($confirm_order_item);
-            foreach ($confirm_order_item as $item){
-                $item->order_id=$order->id;
-                $item->update();
-            }
-            Session::forget($Auth);
-//            dd($request->cc );
             if($request->cc!=null){
                 foreach ($request->cc as $key=>$val){
                     $exists_cc=OrderCc::where('order_id',$order->id)->where('emp_id',$val)->first();
@@ -126,6 +125,46 @@ class OrderController extends Controller
         }else{
             return response()->json(['con'=>true,'msg'=>$validator->errors()]);
         }
+    }
+    public function item_store($request)
+    {
+//        return response($request);
+        $variant = ProductVariations::where('id', $request['variant_id'])->first();
+//        return response($variant);
+        if ($request['type'] == 'invoice') {
+            $sub_total=$request['qty']*$request['price'];
+            $discount=($request['discount']/100)*$sub_total;
+            $total=$sub_total-$discount;
+            $items = new OrderItem();
+            $items->description =$variant->description;
+            $items->quantity =$request['qty'];
+            $items->variant_id = $request['variant_id'];
+            $items->sell_unit = $request['unit_id'];
+            $items->unit_price =$request['price'] ?? 0;
+            $items->total =$total ?? 0;
+            $items->discount_promotion=$request['discount'];
+            $items->creation_id =\Illuminate\Support\Str::random(10);
+            $items->order_id = $request['order_id'] ?? null;
+            $items->state = 1;
+            $items->save();
+        }
+
+    }
+    public function foc_add($request){
+        $variant = ProductVariations::where('id', $request->variant_id)->first();
+        $items = new OrderItem();
+        $items->description = 'This is FOC item';
+        $items->quantity = 1;
+        $items->variant_id = $variant->id;
+        $items->unit_price = 0;
+        $items->sell_unit = $request->unit_id;
+        $items->total = 0;
+        $items->creation_id =\Illuminate\Support\Str::random(10);
+        $items->inv_id = $request->invoice_id;
+        $items->order_id = $request->order_id ?? null;
+        $items->state = 1;
+        $items->foc=true;
+        $items->save();
     }
 
     /**
